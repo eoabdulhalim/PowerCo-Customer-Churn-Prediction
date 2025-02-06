45defd8a8fd07947b6defcb00aa9a0421f2a55e4
Essam Omar <97532581+eoabdulhalim@users.noreply.github.com>
2025-02-06 22:54:23 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import joblib
+import streamlit as st
+import pandas as pd
+from model_production import Model_Pipeline
+import os
+
+
+
+
+
+model = joblib.load(r"D:\1- Statistics and Computer Science\1- My Track\Machine Learning Projects\BCG X Customer Churn Prediction\Final files\model.pkl")
+sc = joblib.load(r"D:\1- Statistics and Computer Science\1- My Track\Machine Learning Projects\BCG X Customer Churn Prediction\Final files\scaler_ohe.pkl")
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(current_dir, 'bgg.jpg')
+st.image(image_path, use_container_width=True)
+
+uploaded_file = st.file_uploader("Upload your New Data of New/Existing Customers to Identify those at Risk of Churn.",type=["csv"])
+
+if uploaded_file is not None:
+    new_data = pd.read_csv(uploaded_file)
+    
+    
+    features = ['channel_sales','cons_12m','cons_gas_12m','cons_last_month','forecast_cons_12m','forecast_cons_year','forecast_discount_energy',
+ 'forecast_meter_rent_12m','forecast_price_energy_off_peak','forecast_price_energy_peak','forecast_price_pow_off_peak','has_gas',
+ 'imp_cons','margin_gross_pow_ele','margin_net_pow_ele','nb_prod_act','net_margin','num_years_antig','origin_up','pow_max',
+ 'var_year_price_off_peak_var','var_year_price_peak_var','var_year_price_mid_peak_var','var_year_price_off_peak_fix','var_year_price_peak_fix',
+ 'var_year_price_mid_peak_fix','var_year_price_off_peak','var_year_price_peak','var_year_price_mid_peak','var_6m_price_off_peak_var',
+ 'var_6m_price_peak_var','var_6m_price_mid_peak_var','var_6m_price_off_peak_fix','var_6m_price_peak_fix','var_6m_price_mid_peak_fix',
+ 'var_6m_price_off_peak','var_6m_price_peak','var_6m_price_mid_peak']
+    
+    
+    result = Model_Pipeline(new_data, features, model, sc)
+    st.write("📈⚙️ Predictions for Uploaded Data:")
+    
+    csv = result.to_csv(index=False)
+    st.download_button(
+        label="📥 Download Predictions",
+        data=csv,
+        file_name='predictions.csv',
+        mime="text/csv"
+        )
