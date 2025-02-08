fc542f8e70e7704509eb58fdeb62478ba7e3a500
Essam Omar <97532581+eoabdulhalim@users.noreply.github.com>
2025-02-08 16:42:45 +0200

    Update model_production.py

--- model_production.py
+++ model_production.py
@@ -9,12 +9,8 @@ Original file is located at
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 import warnings
