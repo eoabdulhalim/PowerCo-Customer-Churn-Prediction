832fa88fbbbb96b684ce9c9033021eb8119fbeef
Essam Omar <97532581+eoabdulhalim@users.noreply.github.com>
2025-02-08 16:53:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ model = joblib.load(r"model.pkl")
 sc = joblib.load(r"scaler_ohe.pkl")
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(current_dir, 'bgg.jpg')
-st.image(image_path, use_container_width=True)
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#image_path = os.path.join(current_dir, 'bgg.jpg')
+#st.image(image_path, use_container_width=True)
 
 uploaded_file = st.file_uploader("Upload your New Data of New/Existing Customers to Identify those at Risk of Churn.",type=["csv"])
 
